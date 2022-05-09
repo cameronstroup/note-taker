8afc5af2a28be50dd5fa1af71733b222d4930796
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-05-09 11:11:31 -0500

    finsished prodcit

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../notes.html"));
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
